897139dca80308564436e44064f45565e77d3c71
WenqiangX <vinjohn@sjtu.edu.cn>
2021-07-01 20:10:25 +0800

    remove obj, urdf related files, for they are not in the same logic

--- rflib/fileio/__init__.py
+++ rflib/fileio/__init__.py
@@ -1,11 +1,10 @@
 # Copyright (c) RobotFlow. All rights reserved.
 from .file_client import BaseStorageBackend, FileClient
-from .handlers import BaseFileHandler, JsonHandler, PickleHandler, YamlHandler, WaveObjHandler, URDFHandler, XacroHandler 
-from .io import dump, load, register_handler
+from .handlers import BaseFileHandler, JsonHandler, PickleHandler, YamlHandler
 from .parse import dict_from_file, list_from_file
 
 __all__ = [
     'BaseStorageBackend', 'FileClient', 'load', 'dump', 'register_handler',
-    'BaseFileHandler', 'JsonHandler', 'PickleHandler', 'YamlHandler', 'WaveObjHandler', 'URDFHandler', 'XacroHandler', 
+    'BaseFileHandler', 'JsonHandler', 'PickleHandler', 'YamlHandler', 
     'list_from_file', 'dict_from_file'
 ]
